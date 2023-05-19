4e6bef97b8bec169ac8b145ce8d4eb3cc84994b7
duduferaso <duduferaso@hotmail.com>
2023-05-18 23:09:58 -0300

    #4 Commit Base

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const FlashcardApp(),
       localizationsDelegates: [
@@ -36,6 +38,13 @@ class Question {
   final String answer;
 
   Question(this.question, this.answer);
+}
+
+class Config {
+  final String buttoncorrect;
+  final String buttonincorrect;
+
+  Config(this.buttoncorrect, this.buttonincorrect);
 }
 
 class Flashcard {
@@ -61,17 +70,25 @@ class FlashcardApp extends StatefulWidget {
 class _FlashcardAppState extends State<FlashcardApp> {
   Flashcard? flashcard;
   bool showAnswer = false;
+  int _selectedIndex = 0;
+  late Config buttonConfig;
+
+  int correctCount = 0;
+  int incorrectCount = 0;
+
+  late Timer _timer;
+  int _secondsRemaining = 20;
 
   @override
   void initState() {
     super.initState();
     loadQuestions();
+    _startTimer();
   }
 
   Future<void> loadQuestions() async {
     final jsonContent =
         await rootBundle.loadString('lib/question/general.json');
-    print('teste' + jsonContent);
     final questionsMap = json.decode(jsonContent) as Map<String, dynamic>;
     final questions = (questionsMap['pt'] as List<dynamic>)
         .map((questionMap) =>
@@ -82,52 +99,142 @@ class _FlashcardAppState extends State<FlashcardApp> {
     });
   }
 
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (_secondsRemaining > 0) {
+        setState(() {
+          _secondsRemaining--;
+        });
+      } else {
+        _timer.cancel();
+        setState(() {
+          showAnswer = true;
+        });
+      }
+    });
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      if (flashcard != null) {
+        final isLastQuestion =
+            flashcard!.currentIndex == flashcard!.questions.length - 1;
+
+        if (isLastQuestion) {
+          // Exibe a mensagem de fim e reinicia os contadores
+          showDialog(
+            context: context,
+            builder: (_) => AlertDialog(
+              title: const Text('Fim das perguntas'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text('Respostas corretas: $correctCount'),
+                  Text('Respostas incorretas: $incorrectCount'),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context); // Fecha o diálogo
+                    _secondsRemaining = 20;
+                    setState(() {
+                      flashcard!.currentIndex =
+                          0; // Reinicia o índice das perguntas
+                      correctCount =
+                          0; // Reinicia o contador de respostas corretas
+                      incorrectCount =
+                          0; // Reinicia o contador de respostas incorretas
+                    });
+                  },
+                  child: const Text('Reiniciar'),
+                ),
+              ],
+            ),
+          );
+        } else {
+          // Incrementa os contadores de acordo com o índice selecionado
+          if (index == 0) {
+            flashcard!.nextQuestion();
+            showAnswer = false;
+            _secondsRemaining = 20;
+            incorrectCount++; // Incrementa o contador de respostas incorretas
+          } else if (index == 1) {
+            flashcard!.nextQuestion();
+            showAnswer = false;
+            _secondsRemaining = 20;
+            correctCount++; // Incrementa o contador de respostas corretas
+          }
+        }
+      }
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flashcards'),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Correct: $correctCount'),
+            Text('$_secondsRemaining s'),
+            Text('Incorrect: $incorrectCount'),
+          ],
+        ),
       ),
       body: flashcard != null
-          ? Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () => setState(() => showAnswer = true),
-                  child: Container(
-                    width: 600,
-                    height: 500,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 2,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                        )
-                      ],
-                    ),
-                    child: Center(
-                      child: Text(
-                        showAnswer
-                            ? flashcard!.currentQuestion.answer
-                            : flashcard!.currentQuestion.question,
-                        style: const TextStyle(fontSize: 20),
+          ? Align(
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GestureDetector(
+                    onTap: () => setState(() => showAnswer = true),
+                    child: Container(
+                      width: 600,
+                      height: 500,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Colors.green,
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: Offset(0, 3),
+                          )
+                        ],
+                      ),
+                      child: Center(
+                        child: Text(
+                          showAnswer
+                              ? flashcard!.currentQuestion.answer
+                              : flashcard!.currentQuestion.question,
+                          style: const TextStyle(fontSize: 20),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                if (showAnswer)
-                  Text(
-                    flashcard!.currentQuestion.answer,
-                    style: const TextStyle(fontSize: 16),
-                  ),
-              ],
+                  const SizedBox(height: 20),
+                  if (showAnswer)
+                    Text(
+                      flashcard!.currentQuestion.question,
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                ],
+              ),
             )
           : const Center(child: CircularProgressIndicator()),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.close), label: 'Incorrect'),
+          BottomNavigationBarItem(icon: Icon(Icons.check), label: 'Correct'),
+        ],
+      ),
     );
   }
 }
